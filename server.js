28d1659848a11f887821aeeba99e3269e8710cdc
Bill Neely <bill.neely.tx@gmail.com>
2016-12-05 15:11:54 -0600

    added Mongo calls - starting part 2

--- server.js
+++ server.js
@@ -3,20 +3,30 @@ const bodyParser = require('body-parser')
 const app = express();
 const MongoClient = require('mongodb').MongoClient
 
-MongoClient.connect('link-to-mongodb', (err, database) +> {
-	// start the server
-})
 
 app.use(bodyParser.urlencoded({extended: true}))
+app.set('view engine', 'ejs')
 
-app.listen(3000, function() {
-	console.log('listening on 3000');
+MongoClient.connect('mongodb://bill:652IcDFOy@ds119788.mlab.com:19788/nodecrud', (err, database) => {
+	if (err) return console.log(err);
+	db = database;
+	app.listen(3000, function() {
+		console.log('listening on 3000');
+	})
 })
 
 app.get('/', (req, res) => {
-	res.sendFile(__dirname + '/index.html')
+	db.collection('quotes').find().toArray((err, result) => {
+		if (err) return console.log(err);
+		res.render('index.ejs', {quotes: result});
+	})
 })
 
+
 app.post('/quotes', (req, res) => {
-	console.log(req.body)
+	db.collection('quotes').save(req.body, (err, result) => {
+		if (err) return console.log(err);
+		console.log('saved to database');
+		res.redirect('/');
+	})
 })
